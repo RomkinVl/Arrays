ce3fbcb97ac869d06195556d74a4af6412c5d58a
XByc_sk76@itstepru.onmicrosoft.com <XByc_sk76@itstepru.onmicrosoft.com>
2022-04-03 20:55:57 +1000

    Home work 02.04.2022

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -1,17 +1,86 @@
 #include <iostream>
 using namespace std;
 
+//#define TASK_8
+//#define TASK_9
+
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int SIZE = 5;
-	int arr[SIZE] = { 3, 5, 8 };
-	//arr[2] = 3; //Обращаемся ко 2-му элементу массива, чтобы записать в него значение "3"
 
+#ifdef TASK_8
+	//Constant
+	const int SIZE = 10;
+
+	//Variables
+	int n, buffer;
+
+	//Arrays
+	int arr[SIZE] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	//Programm
+	cout << "Вывод массива: " << endl;
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << "\t";
 	}
+	cout << endl << endl;
 
+	cout << "На какое число элементов необходимо сдвинуть массив: "; cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		buffer = arr[0];
+		for (int j = 0; j < SIZE - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[9] = buffer;
+	}
 	cout << endl;
+
+	cout << "Результат выполнения: " << endl;
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+#endif // TASK_8
+
+#ifdef TASK_9
+	//Constant
+	const int SIZE = 10;
+
+	//Variables
+	int count;
+
+	//Arrays
+	int arr[SIZE];
+
+	//Programm
+	cout << "Заполните массив любыми целыми числами: " << endl;
+	for (int i = 0; i < SIZE; i++) {
+		cout << "Элемент " << i << " - "; cin >> arr[i];
+	}
+	cout << endl << endl;
+
+	for (int i = 0; i < SIZE; i++) {
+
+		count = 0;
+
+		for (int j = i; j < SIZE; j++) {
+
+			if (arr[i] == arr[j]) count++;
+		}
+
+		if (count > 1) {
+
+			cout << "Количество повторений значения " << arr[i] << " - " << count;
+			cout << endl;
+		}
+
+		count = 0;
+
+	}
+#endif // TASK_9
+		
 }
